cfb138d12fb7f09c9173200c881733cf4d8df052
MFJip612 <sgsssvn@gmail.com>
2026-02-27 18:02:36 +0900

    fix(data): 修正描述中的多余文本

--- src/data/g2nnys.py
+++ src/data/g2nnys.py
@@ -1,6 +1,6 @@
 DATA = {
     "name": "王小美的家",
-    "description": "[单向->] 我只希望在那一刻，回望生命中的无数段故事时，能够问心无愧地告诉自己，我不后悔自己曾做出的每一个选择。",
+    "description": "我只希望在那一刻，回望生命中的无数段故事时，能够问心无愧地告诉自己，我不后悔自己曾做出的每一个选择。",
     "uri": "https://ganyu.rocks",
     "avatar_uri": "https://ganyu.rocks/images/avatar.png"
 }
