f85ee44aa1567f0b367189eeb414d8a4fd663893
MFai612 <test@waterspo.top>
2026-02-27 20:40:07 +0900

    Create test.py

---
+++ src/data/test.py
@@ -0,0 +1,7 @@
+DATA = {
+  "name":"MFJip生活随笔",
+	"description":"Test",
+	"uri": "https://waterspo.top",
+	"avatar_uri": "https://watersop.top/favicon.ico",
+  "fallback_uri": "https://waterspo.top/friends"
+}
