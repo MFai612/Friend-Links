fe641c78c04fcad897d3ef3e9a7ab9c075b762f7
MFai612 <test@waterspo.top>
2026-02-27 19:18:38 +0900

    Update 2x.py

--- src/data/2x.py
+++ src/data/2x.py
@@ -2,5 +2,5 @@ DATA = {
     "name": "二叉树树",
     "description": "Protect What You Love.",
     "uri": "https://2x.nz",
-    "fallback_uri": "https://2x.nz/friends"
+    "fallback_uri": "https://2x.nz/friends/"
 }
